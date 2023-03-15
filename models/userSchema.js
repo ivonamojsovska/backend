72ae9e056992db77b98e40dfacb505348e250cfe
bstrat <benstratton100@gmail.com>
2023-03-15 13:38:30 -0500

    added dependancies for bcrypt and validator, added validation for email and password signup

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,16 @@ const userSchema = new mongoose.Schema({
 
 // Static signup method
 userSchema.statics.signup = async function(email, password) { //needs to be a regular function and not an arrow function
+    // Validation
+    if (!email || !password) {
+        throw Error("All feilds must be filled")
+    }
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid")
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is not strong enough")
+    }
     // Checking to see if an email is already in use
     const exists = await this.findOne({email})
     if (exists) {
