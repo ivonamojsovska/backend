084a6b976054fe7066d4541184bbdc076c36d7dc
king-archy <king.archy.13013@gmail.com>
2023-03-16 19:04:51 -0700

    access control allow cross orgin headers

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const userRoutes = require("./routes/user");
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // Routes
 app.use("/ta/tasks", tasksRoutes);
 app.use("/ta/user", userRoutes);
